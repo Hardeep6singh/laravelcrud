b719a98f61803161d9a58c720e1a37d66a27cff6
Hardeep6singh <hharrys.singh467@gmail.com>
2019-05-03 09:52:02 -0700

    third commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/app.css" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="container"><br><br>
+    <h2><b> Registration form </b></h2>
+     <div class="form-group">
+       <label for=""></label>
+       <input type="text" name="username" id="" class="form-control" placeholder="Enter Your name" aria-describedby="helpId">
+       <br>
+       <input type="email" name="email" id="" class="form-control" placeholder="Enter Your Email" aria-describedby="helpId">
+       <br>
+       <input type="text" name="password" id="" class="form-control" placeholder="Enter Your Passowrd" aria-describedby="helpId">
+       <br>
+       <input type="text" name="password" id="" class="form-control" placeholder="Confirm Password" aria-describedby="helpId">
+       <br>
+       <button type="submit" class="btn btn-primary">Submit</button>
+     </div>
+    </div>
+
+
+</body>
+</html>
